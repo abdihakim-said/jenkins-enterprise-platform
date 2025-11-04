dcc531b6372c691ce30fc814a59d24f767841712
Abdihakim-said <abdihakimsaid1@gmail.com>
2025-11-04 04:19:49 +0000

    Add ec2:CopyImage permission for DR AMI sync
    - Added missing ec2:CopyImage permission to Jenkins IAM policy
    - Fixes UnauthorizedOperation error in Golden AMI pipeline DR sync stage

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -79,8 +79,26 @@ resource "aws_iam_policy" "jenkins" {
           "ec2:DescribeVpcs",
           "ec2:RunInstances",
           "ec2:TerminateInstances",
+          "ec2:StopInstances",
           "ec2:CreateTags",
-          "ec2:DescribeTags"
+          "ec2:DescribeTags",
+          # Packer permissions for AMI building
+          "ec2:CreateKeyPair",
+          "ec2:DeleteKeyPair",
+          "ec2:CreateImage",
+          "ec2:RegisterImage",
+          "ec2:DeregisterImage",
+          "ec2:CopyImage",
+          "ec2:DescribeInstanceTypeOfferings",
+          "ec2:CreateSnapshot",
+          "ec2:DeleteSnapshot",
+          "ec2:DescribeSnapshots",
+          "ec2:ModifyImageAttribute",
+          "ec2:ModifyInstanceAttribute",
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupIngress"
         ]
         Resource = "*"
       },
